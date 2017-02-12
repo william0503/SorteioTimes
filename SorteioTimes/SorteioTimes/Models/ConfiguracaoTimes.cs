4fcd3920e259fe76b9ee6884fa2046a262d09be1
william0503 <william0503@gmail.com>
2017-02-12 00:06:37 -0200

    Add project files.

---
+++ SorteioTimes/SorteioTimes/Models/ConfiguracaoTimes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorteioTimes.Models
+{
+    class ConfiguracaoTimes : BaseModel
+    {
+        public int NumeroTimes { get; set; }
+        public int JogadoresPorTime { get; set; }
+
+        public ConfiguracaoTimes()
+        {
+            NumeroTimes = 0;
+            JogadoresPorTime = 0;
+        }
+
+        public ConfiguracaoTimes(int numeroTimes, int jogadoresPorTime)
+        {
+            NumeroTimes = numeroTimes;
+            JogadoresPorTime = jogadoresPorTime;
+        }
+    }
+}
---
+++ SorteioTimes/SorteioTimes/Models/Time.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorteioTimes.Models
+{
+    public class Time : BaseModel
+    {
+        public IList<string> Jogadores { get; set; }
+
+        public Time()
+        {
+            Jogadores = new List<string>();
+        }
+
+        public void AdicionarJogador(string jogador)
+        {
+            Jogadores.Add(jogador);
+        }
+
+        public void LimparTime()
+        {
+            Jogadores.Clear();
+        }
+    }
+}
